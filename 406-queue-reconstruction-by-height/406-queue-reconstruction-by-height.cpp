db32072295f7d16f284041b3f93b5de28727f330
ARYAN MISHRA <79926995+arrryan05@users.noreply.github.com>
2022-06-29 17:04:39 +0530

    Time: 133 ms (68.94%), Space: 12.2 MB (46.32%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    static bool cmp(vector<int>& p1, vector<int>& p2){
-        if(p1[0]!=p2[0]) return p1[0]>p2[0];
-        else return p1[1]<p2[1];
-    }
 public:
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {        
-        int n = people.size();
-        sort(people.begin(), people.end(), cmp);
-        vector<vector<int>> res;
-        for(int i = 0; i < n; i++){
-            int idx = people[i][1];
-            res.insert(res.begin() + idx, people[i]);
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end());
+        vector<vector<int>> res(people.size(),{-1,-1});
+        for(auto it:people){
+            int idx = it[1],count = 0;
+            for(int i=0;i<people.size();i++){
+                if(count==idx && res[i][0]==-1){
+                    res[i]=it;
+                    break;
+                }
+                if(res[i][0]==-1 || res[i][0]>=it[0]) count++;
+            }
         }
         return res;
     }
