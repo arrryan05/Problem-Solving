79718438c1c3e0d964f5b4dd4038154f2e1f7944
ARYAN MISHRA <79926995+arrryan05@users.noreply.github.com>
2022-07-02 11:03:51 +0530

    Time: 89 ms (74.55%), Space: 32.2 MB (76.96%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -4,9 +4,9 @@ public:
         sort(hCuts.begin(), hCuts.end());
         sort(vCuts.begin(),vCuts.end());
         int max_h = max(hCuts[0], h - hCuts.back());
-        int max_v = max(vCuts[0], w - vCuts.back());
         for (int i = 0; i < hCuts.size() - 1; ++i)
             max_h = max(max_h, hCuts[i + 1] - hCuts[i]);
+        int max_v = max(vCuts[0], w - vCuts.back());
         for (int i = 0; i < vCuts.size() - 1; ++i)
             max_v = max(max_v, vCuts[i + 1] - vCuts[i]);        
         return (long)max_h * max_v % 1000000007;
