6443bd0ef161036f9189336d920262ad254195f3
ARYAN MISHRA <79926995+arrryan05@users.noreply.github.com>
2023-01-12 10:52:19 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,44 +9,44 @@ using namespace std;
 
 class Solution{
 public:
-    // int solve(int i,int w,int val[],int wt[],vector<vector<int>>& dp){
-    //     if(i==0) return ((int)(w/wt[0]))*val[0];
-    //     if(dp[i][w]!=-1) return dp[i][w];
-    //     int nottake=0+solve(i-1,w,val,wt,dp);
-    //     int take = INT_MIN;
-    //     if(wt[i]<=w) take = val[i]+solve(i-1,w-wt[i],val,wt,dp);
-    //     return dp[i][w]=max(take,nottake);
-    // }
-    // int knapSack(int N, int W, int val[], int wt[])
-    // {
-    //     vector<vector<int>> dp(N+1, vector<int>(W+1,-1));
-    //     return solve(N,W,val,wt,dp);
-    // }
-    int knapsackUtil(int wt[], int val[], int ind, int W, vector<vector
-<int>>& dp){
-
-    if(ind == 0){
-        return ((int)(W/wt[0])) * val[0];
+    int solve(int i,int w,int val[],int wt[],vector<vector<int>>& dp){
+        if(i==0) return ((int)(w/wt[0]))*val[0];
+        if(dp[i][w]!=-1) return dp[i][w];
+        int nottake=0+solve(i-1,w,val,wt,dp);
+        int take = INT_MIN;
+        if(wt[i]<=w) take = val[i]+solve(i,w-wt[i],val,wt,dp);
+        return dp[i][w]=max(take,nottake);
     }
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        vector<vector<int>> dp(N, vector<int>(W+1,-1));
+        return solve(N-1,W,val,wt,dp);
+    }
+//     int knapsackUtil(int wt[], int val[], int ind, int W, vector<vector
+// <int>>& dp){
+
+//     if(ind == 0){
+//         return ((int)(W/wt[0])) * val[0];
+//     }
     
-    if(dp[ind][W]!=-1)
-        return dp[ind][W];
+//     if(dp[ind][W]!=-1)
+//         return dp[ind][W];
         
-    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+//     int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
     
-    int taken = INT_MIN;
-    if(wt[ind] <= W)
-        taken = val[ind] + knapsackUtil(wt,val,ind,W-wt[ind],dp);
+//     int taken = INT_MIN;
+//     if(wt[ind] <= W)
+//         taken = val[ind] + knapsackUtil(wt,val,ind,W-wt[ind],dp);
         
-    return dp[ind][W] = max(notTaken,taken);
-}
+//     return dp[ind][W] = max(notTaken,taken);
+// }
 
 
-int knapSack(int N, int W, int val[],int wt[]) {
+// int knapSack(int N, int W, int val[],int wt[]) {
     
-    vector<vector<int>> dp(N,vector<int>(W+1,-1));
-    return knapsackUtil(wt, val, N-1, W, dp);
-}
+//     vector<vector<int>> dp(N,vector<int>(W+1,-1));
+//     return knapsackUtil(wt, val, N-1, W, dp);
+// }
 
 
 };
